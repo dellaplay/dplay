67e71217e3c270bbca616603fe8f740282cb90ac
Anderz Lindberg <Anderz Lindberg>
2017-07-09 16:37:49 +0200

    20170709_002justTesting22 äta sen annat ett tag

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>DellaPlayJustFockingPlaying</title>
-
+<!--
 		<script src="jquery.js"></script>
-		
+-->		
 		<link rel="stylesheet" href="CSS/main.css" />
 	</head>
 	
@@ -13,7 +13,7 @@
 		<header class="CENTERTXT">
 			<h1>DellaPlay</h1>
 			<p class="LEFTTXT">
-				Men inte klart p&aring; hundratusen &aring;r, minst!
+				Men inte klart p&aring; hundratusen &aring;r, minst! Så ta det för vad det är!
 			</p>
 			<nav>
 				NAV
@@ -23,16 +23,23 @@
 		
 		<section>
 			<article>
-				None yet!
+				<h3>#1 Bosman</h3>
+				<p>
+					I det allra första avsnittet pratar vi om hur absurt det är att man inte får 
+					äga spelare längre. OCH om ett slagsmål i hockeymatchen från igår.
+				</p>
+				<audio controls>
+					<source src="AUDIO/00001.mp3" type="audio/mpeg">
+					Din webbläsare klarar inte ens mp3, fan vad lökigt!
+					</audio>
 			</article>
 		</section>
 		
 		<footer class="CENTERTXT">
 				FOOTER
 		</footer>
-
+<!--
 		<script>
-
 			$( document ).ready(function() 
 			{
 				/*$("").(function() 
@@ -40,9 +47,8 @@
 					
 				});*/
 			});
-
 		</script>
-
+-->
 
 	</body>
 </html>
