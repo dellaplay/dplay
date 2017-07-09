c86b3f60c844ff6a86d4ec7153380452b077e41b
Anderz Lindberg <Anderz Lindberg>
2017-07-09 16:16:48 +0200

    20170709_002justTesting21 one dot costed two hours IDIOOOOT jag alltså

--- index.php
+++ index.php
@@ -3,28 +3,36 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>DellaPlayJustFockingPlaying</title>
+
 		<script src="jquery.js"></script>
+		
 		<link rel="stylesheet" href="CSS/main.css" />
 	</head>
 	
 	<body>
-		<header>
-			
+		<header class="CENTERTXT">
+			<h1>DellaPlay</h1>
+			<p class="LEFTTXT">
+				Men inte klart p&aring; hundratusen &aring;r, minst!
+			</p>
+			<nav>
+				NAV
+			</nav>
 		</header>
 		
-		<nav>
-			
-		</nav>
 		
 		<section>
-		
+			<article>
+				None yet!
+			</article>
 		</section>
 		
-		<footer>
-			
+		<footer class="CENTERTXT">
+				FOOTER
 		</footer>
-		
+
 		<script>
+
 			$( document ).ready(function() 
 			{
 				/*$("").(function() 
@@ -32,6 +40,9 @@
 					
 				});*/
 			});
+
 		</script>
+
+
 	</body>
 </html>
